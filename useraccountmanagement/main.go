2279b3233442d373468fcf54595a6d4dfe1de38a
varma1506 <durganarayanavarma@gmail.com>
2023-03-31 23:28:23 -0400

    auth test

--- useraccountmanagement/main.go
+++ useraccountmanagement/main.go
@@ -4,14 +4,15 @@ import (
 	"fmt"
 	"net/http"
 
+	auth "github.com/Varma1506/user-account-management/auth"
 	routes "github.com/Varma1506/user-account-management/routes"
 )
 
 func main() {
 
 	http.HandleFunc("/signup", routes.Signup)
-	http.HandleFunc("/changepassword", routes.ChangePassowrd)
-	http.HandleFunc("/deleteaccount/", routes.DeleteAccount)
+	http.HandleFunc("/changepassword", auth.Authenticate(routes.ChangePassowrd))
+	http.HandleFunc("/deleteaccount/", auth.Authenticate(routes.DeleteAccount))
 	http.HandleFunc("/login", routes.Login)
 	fmt.Println("SUccess")
 	http.ListenAndServe(":8000", nil)
